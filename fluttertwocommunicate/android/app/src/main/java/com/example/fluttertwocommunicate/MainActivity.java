b76bca58c7c2ab1e12ad4dc5d2d2937aeb2ee18b
iBinbro <812117956@qq.com>
2019-01-31 16:46:49 +0800

    补充原生的安卓与flutter的交互传值

--- fluttertwocommunicate/android/app/src/main/java/com/example/fluttertwocommunicate/MainActivity.java
+++ fluttertwocommunicate/android/app/src/main/java/com/example/fluttertwocommunicate/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.fluttertwocommunicate;
 
 import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+
+    private static final String CHANNEL = "https://www.jianshu.com/u/ee3db73e5459";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
+
+        new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(new MethodCallHandler() {
+            @Override
+            public void onMethodCall(MethodCall call, Result result) {
+                if (call.method.equals("callNativeMethond")) {
+
+                    Object para = call.arguments;
+                    System.out.println("flutter传给安卓的值：" + para);
+
+                    String nativeFinalStr = "返回给原生安卓的值";
+
+                    result.success(nativeFinalStr);
+
+                    result.error("001", "安卓进入异常", "进入flutter的trycatch方法的catch方法");
+
+                } else {
+                    result.notImplemented();
+                }
+            }
+        });
+    }
 }
