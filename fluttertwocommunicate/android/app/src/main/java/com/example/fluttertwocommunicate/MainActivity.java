e1d50e8d4872d40a41857f9c02f0309ee7048cf3
iBinbro <812117956@qq.com>
2019-02-13 14:19:11 +0800

    安卓端java代码补充 原生主动发送信息给flutter完成

--- fluttertwocommunicate/android/app/src/main/java/com/example/fluttertwocommunicate/MainActivity.java
+++ fluttertwocommunicate/android/app/src/main/java/com/example/fluttertwocommunicate/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.fluttertwocommunicate;
 
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.StreamHandler;
+
+import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugins.GeneratedPluginRegistrant;
@@ -11,6 +17,15 @@ import io.flutter.plugin.common.MethodChannel.Result;
 public class MainActivity extends FlutterActivity {
 
     private static final String CHANNEL = "https://www.jianshu.com/p/ce7ed8bbf35c";
+
+    private static final String NativeToFlutterChannel = "https://www.jianshu.com/p/7dbbd3b4ce32";
+
+    int _nativeCount = 0;
+    //安卓原生方法传参，每1s就会被执行一次实现自增
+    private void repeatCount(EventChannel.EventSink eventSink){
+        _nativeCount ++;
+        eventSink.success(_nativeCount);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +42,7 @@ public class MainActivity extends FlutterActivity {
 
                     String nativeFinalStr = "返回给原生安卓的值";
 
-//                    result.success(nativeFinalStr);
+                    result.success(nativeFinalStr);
 
                     result.error("001", "安卓进入异常", "进入flutter的trycatch方法的catch方法");
 
@@ -36,5 +51,29 @@ public class MainActivity extends FlutterActivity {
                 }
             }
         });
+
+        //注册监听通道
+        new EventChannel(getFlutterView(), NativeToFlutterChannel).setStreamHandler(
+                new EventChannel.StreamHandler() {
+                    Timer timer = new Timer();
+
+                    @Override
+                    //注册成功后的回调
+                    public void onListen(Object o, EventChannel.EventSink eventSink) {
+                        //定时器自增数值然后传参
+                        timer.schedule(new TimerTask() {
+                            @Override
+                            public void run() {
+                                repeatCount(eventSink);
+                            }
+                        }, 0, 1000);
+                    }
+
+                    @Override
+                    public void onCancel(Object o) {
+                        timer = null;
+                    }
+                }
+        );
     }
 }
